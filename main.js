e7a02982e9fac3e0f297ca4e04740c7c83dad3d8
Donghwan Kim <donghwan.kim92@gmail.com>
2021-08-06 01:01:31 +0200

    fourth

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ const countBtn = document.getElementById("countBtn");
 countBtn.addEventListener('click', () => {
     let input = document.getElementById('input').value;
     // console.log(vowelCount(input));
-    vowelCount(input);
+    alert(vowelCount(input));
 });
 
 function vowelCount(str) {
+    var count = 0;
+    var vowels = "aeiouAEIOU";
     for (var i = 0; i < str.length; i++) {
-        console.log(str[i]);
+        if (vowels.indexOf(str[i]) > -1) {
+            count++;
+        }
     }
+    return "Your texts contains " + count + " vowels in it.";
 };
